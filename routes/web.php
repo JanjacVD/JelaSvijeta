c425fa8ec58d74379042eb54139de2e30ae82c61
JanjacVD <valentino.janjac@gmail.com>
2022-06-09 02:54:36 +0200

    Officaly 2:45 AM

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function(){
-    return 'Hey, welcome! <br/> I did my best to do this so I hope you somewhat enjoy exploring this code, in case you have any questions feel free to email me or call me :)
+    return "Hey, welcome! <br/> I did my best to do this so I hope you somewhat enjoy exploring this code, in case you have any questions feel free to email me or call me :)
     <br/>
-    Now please procceed to the route "localhost:port/api" and go on from there. Thank you for your time!
-    ';
+    Now please procceed to the route 'localhost:port/api' and go on from there. Thank you for your time!
+    Note: I just came from work and it's almost 3 am when I'm doing this, but I did my best to make things work,
+    Also a note:
+    You'll probably be reading this, and we haven't met or talked directly, but I want to just let you know that, I honestly really appreciate the amnout of effort
+    that you've put into testing, and especially your reviews because due to them I've learned so much. Al dosta s engleskim, ako išta, bar dugujem rundu, dvije :)
+    ";
 });
